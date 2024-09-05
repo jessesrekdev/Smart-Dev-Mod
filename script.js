ee7cbe9a9987c15d0ec13d083f502eca31dd09de
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-09-05 18:34:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Placeholder for future actions when cards are clicked
+document.getElementById('base64-converter').addEventListener('click', function() {
+  console.log('Base 64 Converter selected.');
+});
+
+document.getElementById('toast-maker').addEventListener('click', function() {
+    console.log('Smart Toast Maker selected.');
+});
