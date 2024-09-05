9ddd20d83f72880be508ab516992c019a7903893
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-09-05 18:45:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Placeholder for future actions when cards are clicked
 document.getElementById('base64-converter').addEventListener('click', function() {
   console.log('Base 64 Converter selected.');
+  window.open('tools/Base 64 Converter/index.html');
 });
 
 document.getElementById('toast-maker').addEventListener('click', function() {
+window.open('tools/Base 64 Converter/index.html');
     console.log('Smart Toast Maker selected.');
 });
