0e74a875badf6514533fc02ce08a8c478e966620
jessesrekdev <159046986+jessesrekdev@users.noreply.github.com>
2024-09-06 18:12:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-// Placeholder for future actions when cards are clicked
+// JavaScript
 document.getElementById('base64-converter').addEventListener('click', function() {
   console.log('Base 64 Converter selected.');
   window.open('tools/Base 64 Converter/index.html');
 });
 
 document.getElementById('toast-maker').addEventListener('click', function() {
-window.open('tools/Smart Dev Toast Marker/index.html');
+  document.getElementById('modal').style.display = 'block';
+});
+
+document.getElementById('smali-toast').addEventListener('click', function() {
+  document.getElementById('modal').style.display = 'none';
+  setTimeout(() => window.open('tools/Smart Dev Toast Marker/index.html'), 10);
+});
+
+document.getElementById('smali-toast-color').addEventListener('click', function() {
+  document.getElementById('modal').style.display = 'none';
+  setTimeout(() => window.open('tools/Smart Dev Toast with color/index.html'), 10);
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === document.getElementById('modal')) {
+    document.getElementById('modal').style.display = 'none';
+  }
 });
